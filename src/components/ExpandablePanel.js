00e8e4d00b3317cf3c5dd4aa676fa4ce14430fc6
Marcia Moss <marcia_moss@hotmail.com>
2023-01-11 03:36:12 -0500

    adjusted text alignment for expanded panel

--- src/components/ExpandablePanel.js
+++ src/components/ExpandablePanel.js
@@ -9,7 +9,7 @@ function ExpandablePanel({ header, children }) {
   };
 
   return (
-    <div className="mb-2 border rounded">
+    <div className="mb-2 border rounded text-left">
       <div className="flex p-2 justify-between items-center">
         <div className="flex flex-row items-center justify-between">
           {header}
