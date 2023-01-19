29e1ee7c62ca4eb5dafbddbbfe53fa66c65617ed
Marcia Moss <marcia_moss@hotmail.com>
2023-01-19 05:24:39 -0500

    display news date and news image

--- src/components/NewsPage/NewsListItem.js
+++ src/components/NewsPage/NewsListItem.js
@@ -4,12 +4,14 @@ import ExpandablePanel from '../ExpandablePanel';
 import ConfirmModal from '../ConfirmModal';
 import { BsFillBookmarkHeartFill, BsFillBookmarkDashFill } from 'react-icons/bs';
 import { FaInfoCircle } from 'react-icons/fa';
+import { BiNews } from 'react-icons/bi';
 import { saveNews, removeNews, resetNewsSaveSuccess } from '../../store';
 
 function NewsListItem({ news, userId, bookmarked }) {
   const dispatch = useDispatch();
   const [previouslySaved, setPreviouslySaved] = useState(false);
   const [deleteConfirm, setDeleteConfirm] = useState(false);
+  const [rDate, setRDate] = useState('');
   const {savedUri, savedNews} = useSelector((state) => {
     return {
       savedNews: state.news.savedNews,
@@ -29,6 +31,15 @@ function NewsListItem({ news, userId, bookmarked }) {
     }
   }, [savedUri, news.uri, dispatch, resetAlert]);
 
+  useEffect(() => {
+    setRDate(news.published_date);
+    if(news.published_date) {
+      const dt = new Date(news.published_date);
+      const dte = (dt.getDate()).toString().length === 1 ? `0${dt.getDate()}` : dt.getDate();
+      setRDate(`${dte}${new Intl.DateTimeFormat("en-US", { month: "short" }).format(dt)}${dt.getFullYear()}`);
+    }
+  }, [news.published_date]);
+
   const handleClick = () => {
     if(!bookmarked) {
       if(!((savedNews.filter(s=>s.uri===news.uri)).length > 0)) {
@@ -47,7 +58,7 @@ function NewsListItem({ news, userId, bookmarked }) {
       <button className="mr-3" onClick={handleClick}>
         {!bookmarked ? <BsFillBookmarkHeartFill/> : <BsFillBookmarkDashFill/>}
       </button>
-      {news.title}, {news.byline}
+      {news.title}, {news.byline}, {rDate}
     </>
   );
 
@@ -60,7 +71,13 @@ function NewsListItem({ news, userId, bookmarked }) {
           <p className="ml-1">{previouslySaved ? `Previously Bookmarked: "${news.title}"` : `Bookmarked "${news.title}"`}</p>
         </div> : ''}
       <ExpandablePanel header={header}>
-        <div className="text-xl">{news.abstract},
+        <div className="text-xl">
+        <div className="text-left">
+          {!news.multimedia ?
+            <a href={news.short_url} target="blank" className="italic text-blue-600"><img className="inline h-20 w-20" src={news.multimedia[0].url} alt="N/A"/></a>
+            : <a href={news.short_url} target="blank" className="italic text-blue-600"><BiNews className="inline h-20 w-20"/></a>}
+          </div>
+          {news.abstract},
           <a href={news.short_url} target="blank" className="ml-1 italic text-blue-600 visited:text-purple-600">
           <br/>Click here to read the full article
           </a>
