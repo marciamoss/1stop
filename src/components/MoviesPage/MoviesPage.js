38549f8149615acdffaeb504212f98949976a235
Marcia Moss <marcia_moss@hotmail.com>
2023-02-22 06:10:18 -0500

    removed commented code

--- src/components/MoviesPage/MoviesPage.js
+++ src/components/MoviesPage/MoviesPage.js
@@ -18,67 +18,6 @@ const MoviesPage = ({ bookmarkedPage }) => {
   });
 
   return (
-    // <div className="movies-page-content">
-    //   <div className="form container">
-    //     <h5 className="text-right text-blue-600">
-    //       {!bookmarkedPage ? (
-    //         <Link
-    //           className="text-blue-900 italic font-bold text-2xl"
-    //           to="/movies/bookmarked"
-    //         >
-    //           Bookmarked
-    //         </Link>
-    //       ) : (
-    //         <Link
-    //           className="text-blue-900 italic font-bold text-2xl"
-    //           to="/movies"
-    //         >
-    //           Back to Search
-    //         </Link>
-    //       )}
-    //     </h5>
-    //     {bookmarkedPage ? (
-    //       <MoviesList bookmarked={true} />
-    //     ) : (
-    //       <>
-    //         <div>
-    //           <h2 className="text-2xl font-bold mb-1">Find a movie</h2>
-    //           <div className="flex justify-center">
-    //             <form
-    //               onSubmit={(event) => event.preventDefault()}
-    //               className="relative w-3/4"
-    //             >
-    //               <input
-    //                 type="text"
-    //                 className="h-14 w-full pl-14 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-    //                 placeholder="Movie Title (Required)"
-    //                 value={movieTitle}
-    //                 onChange={(event) => dispatch(setMovie(event.target.value))}
-    //               />
-    //               <div className="absolute top-2 left-2">
-    //                 <Button
-    //                   disabled={!movieTitle}
-    //                   loading={isLoading}
-    //                   onClick={() =>
-    //                     dispatch(searchMovie(movieTitle, fetchMovies))
-    //                   }
-    //                   className={`h-10 w-fit text-white rounded-lg ${
-    //                     movieTitle
-    //                       ? "bg-blue-900 hover:bg-green-900"
-    //                       : "bg-gray-100 text-slate-300"
-    //                   }  ${isLoading ? "bg-green-900" : ""}`}
-    //                 >
-    //                   <BsSearch size={25} />
-    //                 </Button>
-    //               </div>
-    //             </form>
-    //           </div>
-    //         </div>
-    //         <MoviesList list={moviesList} bookmarked={false} />
-    //       </>
-    //     )}
-    //   </div>
-    // </div>
     <div className="container max-[770px]:text-sm text-xl text-center mt-2 p-2">
       <h5 className="text-right">
         {!bookmarkedPage ? (
